3be11e855c4b860afc7c14efec2d1e698b67c8dc
KeshaPatel7 <keshapatel@github.com>
2023-03-16 16:39:24 -0400

    made changes

--- multilingual_greeter.py
+++ multilingual_greeter.py
@@ -24,6 +24,16 @@ def chosen_language():
 chosen_language()
 
 
+# def language_input():
+#     chosen_language = input("Please choose your language (1 for English, 2 for Spainish, 3 for French)")
+#     if chosen_language == "1":
+#         print("Hello " + input("What's your name?"))
+#     elif chosen_language == "2":
+#         print("Hola " + input("Cómo te llamas?"))
+#     elif chosen_language == "3":
+#         print("Bonjour " + input("Comment vous-appelez vous?"))
+
+
 
 
 
