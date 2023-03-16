1c21d8465cc03c4a380bf6e49a8da957e9c0f66a
KeshaPatel7 <keshapatel@github.com>
2023-03-16 16:35:57 -0400

    excercise3 done

---
+++ multilingual_greeter.py
@@ -0,0 +1,31 @@
+ # def greet(name):
+ #    print("Hello " +name)
+ #
+ # def name_input():
+ #    name1 = input("What's your name?")
+ #    return name1
+ #
+ # print(name_input()+" Patel")
+
+
+def chosen_language():
+    print("Please choose a language:")
+    print("1.English")
+    print("2.Hindi")
+    print("3.Gujarati")
+    chosen = (input())
+
+    if chosen == "3":
+        print("તમારું નામ શું છે?\n")
+    elif chosen == "2":
+        print("आपका क्या नाम है?\n")
+    else:
+        print("what's your name?\n")
+chosen_language()
+
+
+
+
+
+
+
